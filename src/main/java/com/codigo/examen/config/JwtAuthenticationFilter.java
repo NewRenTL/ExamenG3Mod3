7e4399388eac1e1913dc5ee8a69d9e8feae27ce8
NewRenTL <diego.bustamante@utec.edu.pe>
2024-03-11 17:57:38 -0500

    subidaprev

---
+++ src/main/java/com/codigo/examen/config/JwtAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package com.codigo.examen.config;
+
+import com.codigo.mslogin.service.JWTService;
+import com.codigo.mslogin.service.UsuarioService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Objects;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JWTService jwtService;
+    private final UsuarioService usuarioService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+
+        if(StringUtils.isEmpty(authHeader) || !StringUtils.startsWithIgnoreCase(authHeader,"Bearer"))
+        {
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUserName(jwt);
+
+        //Reviso que no este logeado y que no sea nulo el email recuperador del token
+        if(Objects.nonNull(userEmail) && SecurityContextHolder.getContext().getAuthentication() == null)
+        {
+            UserDetails userDetails = usuarioService.userDetailsService().loadUserByUsername(userEmail);
+            if(jwtService.validateToken(jwt,userDetails))
+            {
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+                //No hay credenciales porque es autentificacion por medio de un token
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,null,userDetails.getAuthorities()
+                );
+
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                securityContext.setAuthentication(authenticationToken);
+                SecurityContextHolder.setContext(securityContext);
+            }
+        }
+        filterChain.doFilter(request,response);
+
+    }
+}
